6916a57d094b537bd4727fb98af42c2ec89855a0
hye5nee123 <gpdnjsl123@naver.com>
2023-12-25 13:47:36 +0900

    Test

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+
+import java.text.SimpleDateFormat;
+
+public class Board {
+    //필드
+    private int boardNo;
+    private String boardHead;
+    private String boardWho;
+    private String board
+    public static void main(String[] args) {
+
+    }
+}
