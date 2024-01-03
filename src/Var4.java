7d6b5c71d7dc8dd8f72aca4e921f4ed616dfa625
hye5nee123 <gpdnjsl123@naver.com>
2024-01-03 22:41:50 +0900

    compile error

---
+++ src/Var4.java
@@ -0,0 +1,7 @@
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c,d;
+    }
+}
---
+++ src/Var5.java
@@ -0,0 +1,19 @@
+public class Var5 {
+    public static void main(String[] args) {
+        // 1.변수 선언, 초기화 각각 따로.
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        int b = 2; //선언과 초기화 한 번에.
+        System.out.println(b);
+
+        int c = 3, d = 4; //여러 번수 선언과 초기화 한 번에.
+        System.out.println(c);
+        System.out.println(d);
+
+        //변수를 꼭 초기화 해야 하는 이유.?
+
+
+    }
+}
