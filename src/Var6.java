ae0664c525a5f976aef7b48fb634906bcfb9b557
hye5nee123 <gpdnjsl123@naver.com>
2024-01-04 22:56:25 +0900

    ~13강 (변수 선언 방식)

--- src/Var6.java
+++ src/Var6.java
@@ -1,12 +1,12 @@
 public class Var6 {
     public static void main(String[] args) {
         int a;
-        System.out.println(a);
+        //System.out.println(a);
         /**java: variable a might not have been initialized
         //> 초기화 x
          java는 메모리 공간에 어떤 것이 있을지 몰라 초기화 하는 것을 강제.무조건 따라야 함.
-         컴파일 에러?! output에 없음
+         컴파일 에러?! output에 없음*/
 
 
-    }
+         }
 }
---
+++ src/Var7.java
@@ -0,0 +1,29 @@
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b =10.5; //실수
+
+        boolean c = true; //true false만 입력 가능
+        char d = 'A'; //문자 하나.
+        String e ="Hello java"; //문자열. 문자열을 다루기 위한 특별한 타입.
+
+
+        // int z ="백원";  error
+        int z = 100; //ㄱㄴ
+
+        /**리터럴
+         * 문자 그 자체.(개발자가 직접 적어놓은 값)
+         *
+         * int a = 100 에서 '100'은 '100'의 값을 지닌 '리터럴'이다.
+         * 변수의 값은 바뀔 수 있지만 리터럴은 바뀔 수 없다. >몇 번 더 보면 이해 갈 듯;
+         */
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        //ctrl +d => 복붙
+    }
+}
